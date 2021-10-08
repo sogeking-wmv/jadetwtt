93623a5bdb95053f60f79d702f51bc596e650e51
sogeking-wmv <77034977+sogeking-wmv@users.noreply.github.com>
2021-10-07 23:06:47 -0300

    Add files via upload

---
+++ jadetttwtt/config.py
@@ -0,0 +1,7 @@
+# Keys do bot
+
+consumer_key = "a3s78XSJEYxKpOT0h0bIT1VSX"
+consumer_secret = "UUvFIaed2dm7jreLRmbNOlxloNoyrzOAtU6UV0gyNzSv9DQQ2Q"
+
+access_token = "1360217583375568897-PffmfByEzwbhfSWVpZJ8Nup99SopwK"
+access_token_secret = "cNzaBY8tEAiwVoIxEoxgdN8ANcdhyQC24CqiRJ335qBRd"
---
+++ jadetttwtt/main.py
@@ -0,0 +1,24 @@
+import tweepy
+import schedule
+import time
+
+# Importa as keys em config.py
+from config import *
+
+# Autentizar
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Coisa de API
+api = tweepy.API(auth)
+
+
+def job():
+    api.update_with_media(filename="bot1imgsol.jpg", status="Bom dia, @jadexxw! 🌸⛅\n\nCom você ao meu lado sei que todos os dias podem ser perfeitos. Tenha um ótimo dia!")
+    return
+
+schedule.every().day.at("15:40").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
